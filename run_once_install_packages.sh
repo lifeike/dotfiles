65e7bb633d98c00d0536c4a2a06b1c1daab4185a
lifeike <lifeike67@gmail.com>
2023-07-23 17:46:01 -0600

    Update install_packages.sh

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,5 +1,5 @@
 sudo apt install -y git
-sudo apt remove -y nano
-sudo apt-get install -y vim-gtk
+#sudo apt remove -y nano
+#sudo apt-get install -y vim-gtk
 #sudo apt install -y zathura
 #sudo apt install -y mpv
