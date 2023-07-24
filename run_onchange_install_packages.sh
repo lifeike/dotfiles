abbc91061b4859a1856b0bfa1d0cf6dd1240bf19
lifeike <lifeike67@gmail.com>
2023-07-24 10:44:35 -0600

    fix

--- run_onchange_install_packages.sh
+++ run_onchange_install_packages.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-sudo apt             -y update
-sudo apt             -y upgrade
+sudo apt             -y update           &> /dev/null
+sudo apt             -y upgrade          &> /dev/null
 
 # install packages
-sudo apt     install -y ranger
-sudo apt     install -y git
-sudo apt     install -y tmux
+sudo apt     install -y ranger           &> /dev/null
+sudo apt     install -y git              &> /dev/null
+sudo apt     install -y tmux             &> /dev/null
 
 # editor 
-sudo apt     remove  -y nano
-sudo apt-get install -y vim-gtk
+sudo apt     remove  -y nano             &> /dev/null    
+sudo apt-get install -y vim-gtk          &> /dev/null
 # sudo snap install --classic code
 
 # media
-sudo apt     install -y mpv
+sudo apt     install -y mpv              &> /dev/null
 
 # pdf
-sudo apt     install -y zathura
+sudo apt     install -y zathura          &> /dev/null
 
 # boxes
-sudo apt-get install -y gnome-boxes
+sudo apt-get install -y gnome-boxes      &> /dev/null
 
 # system tool
-sudo apt     install -y python3-pip
-sudo pip     install --upgrade glances
+sudo apt     install -y python3-pip      &> /dev/null
+sudo pip     install --upgrade glances   &> /dev/null
 
 # load source files
 cd $HOME;
