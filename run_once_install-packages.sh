94353184cdcf5ea169e9061f9be488189c2e0d86
Feeco <lifeike67@gmail.com>
2025-04-07 22:30:35 -0600

    ad

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -74,8 +74,8 @@ echo "##############################################"
 echo "chrome"
 echo "##############################################"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install google-chrome-stable_current_amd64.deb
-rm   google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm   ./google-chrome-stable_current_amd64.deb
 
 echo "##############################################"
 echo "wechat"
