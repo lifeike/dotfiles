94f94136e10f62a013b229ef1b668ba61af2f7bf
Feeco <lifeike@gmail.com>
2025-04-12 17:06:26 -0600

    Update .tmux.conf Update .config/nvim/lua/mappings.lua Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -9,7 +9,7 @@ map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- Feeco's custom mappings
 -- Telescope
-map({"n","i","v"}, "<leader>o", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
+map({"n","i","v"}, "<C-o>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
 -- Telescope Find Files
 map({"n","i","v"}, "<A-i>", "<cmd>Telescope find_files<CR>", { desc = "find files" })
 -- Telescope Find Occurrence
@@ -22,6 +22,15 @@ map({"n","i","v"}, "<A-w>", "<cmd>bd<CR>", { desc = "close current tab" })
 map({ "n", "t","i","v" }, "<A-Space>", function()
   require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
 end, { desc = "terminal toggle floating term" })
+
+map({ "n", "t" }, "<A-S-h>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggleable horizontal term" })
+
+map({ "n", "t" }, "<A-S-v>", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggleable vertical term" })
+
 -- Navigation
 map("n", "<C-j>", "10j",{ desc = "alt+j jump 10 lines down" })
 map("n", "<C-k>", "10k",{ desc = "alt+k jump 10 lines up" })
