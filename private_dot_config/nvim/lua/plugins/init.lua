017a3155320d89222ee0fc153d46118117370d69
Feeco <lifeike67@gmail.com>
2025-04-12 17:06:26 -0600

    Update .tmux.conf Update .config/nvim/lua/mappings.lua Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -22,4 +22,19 @@ return {
   -- 		},
   -- 	},
   -- },
+  
+
+
+  -- feeco's plugins
+  {
+   "Pocco81/auto-save.nvim",
+   lazy=false,
+	  config = function()
+		 require("auto-save").setup {
+			-- your config goes here
+			-- or just leave it empty :)
+		 }
+	  end,
+  },
+
 }
