4baf40012ab90877176db5ff58fb6f2a4e064da4
Feeco <lifeike67@gmail.com>
2025-04-12 13:15:44 -0600

    Update .config/nvim/lua/mappings.lua

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -1,6 +1,5 @@
 require "nvchad.mappings"
 
--- add yours here
 
 local map = vim.keymap.set
 
@@ -8,3 +7,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Feeco's custom mappings
+-- Telescope
+map("n", "<A-t>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
+map("i", "<A-t>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
+map("v", "<A-t>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
+-- Telescope Find Files
+map("n", "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "show all commands in telescope" })
+map("i", "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "show all commands in telescope" })
+map("v", "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "show all commands in telescope" })
