02a152f140df1576f9b4fd638686d91c0d1862b5
Feeco <lifeike@gmail.com>
2025-04-12 13:34:47 -0600

    Update .config/nvim/lua/mappings.lua

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -9,10 +9,12 @@ map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- Feeco's custom mappings
 -- Telescope
-map("n", "<A-t>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
-map("i", "<A-t>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
-map("v", "<A-t>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
+map({"n","i","v"}, "<A-c>", "<cmd>Telescope<CR>", { desc = "show all commands in telescope" })
 -- Telescope Find Files
-map("n", "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "show all commands in telescope" })
-map("i", "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "show all commands in telescope" })
-map("v", "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "show all commands in telescope" })
+map({"n","i","v"}, "<A-f>", "<cmd>Telescope find_files<CR>", { desc = "find files" })
+-- General Find Files
+map({"n","i","v"}, "<A-s>", "<cmd>w<CR>", { desc = "general save file" })
+-- Termninal
+map({ "n", "t","i","v" }, "<A-Space>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
