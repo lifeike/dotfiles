750df6a8151c91105fcdf71d585faeb5b7b6b9d8
Feeco <lifeike67@gmail.com>
2025-04-13 10:57:54 -0600

    Add .config/nvim/lua/snippets/typescriptreact.lua

---
+++ private_dot_config/nvim/lua/snippets/typescriptreact.lua
@@ -0,0 +1,18 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+  s("div", {
+    t("<div>"),
+    i(1),
+    t("</div>"),
+  }),
+  s("<>", {
+    t("<>"),
+    i(1),
+    t("</>"),
+  }),
+}
+
