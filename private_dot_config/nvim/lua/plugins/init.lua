0c395c15a30ceaa55c8200527c72edfffeeb45ef
Feeco <lifeike@gmail.com>
2025-04-15 11:15:30 -0600

    Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -30,11 +30,11 @@ return {
   {
    "Pocco81/auto-save.nvim",
    lazy=false,
-	  config = function()
-		 require("auto-save").setup {
-			-- your config goes here
-			-- or just leave it empty :)
-		 }
-	  end,
+	 config = function()
+	  require("auto-save").setup {
+	 	-- your config goes here
+	 	-- or just leave it empty :)
+	  }
+	 end,
   },
 }
