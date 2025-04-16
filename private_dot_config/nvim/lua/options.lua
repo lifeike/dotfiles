624ea301aee615bea3e053133e866a6012996434
Feeco <lifeike@gmail.com>
2025-04-16 10:08:35 -0600

    Update .config/nvim/lua/options.lua

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -4,4 +4,4 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline and row number!
-o.cursorcolumn =false    -- to enable vertical cursorline!
+o.cursorcolumn =false   -- I tried vertical cursor line, finally disable it, cuz looks weird
