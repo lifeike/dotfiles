38b448e75d9b616609c6a664c85a18a9f0254043
Feeco <lifeike67@gmail.com>
2025-04-20 00:27:18 -0600

    Update .config/nvim/lua/chadrc.lua

--- private_dot_config/nvim/lua/chadrc.lua
+++ private_dot_config/nvim/lua/chadrc.lua
@@ -1,17 +1,20 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "ayu_dark",
+  theme = "ayu_dark",
   transparency = true,
-	hl_override = {
-		CursorLine = { bg = '#4D4D4D' },
-		CursorColumn = { bg = '#333333' },
-	},
+  hl_override = {
+    CursorLine = { bg = '#4D4D4D' },
+    CursorColumn = { bg = '#333333' },
+    Comment = { fg = '#00ffff' },
+    -- Changes color of comments when Treesitter is active
+    ["@comment"] = { fg = "#00ffff" },
+  },
 }
 
 -- M.nvdash = { load_on_startup = true }
