0ea7c88682d78b85ff04127e38771e9bc3c4aa7b
Feeco <lifeike@gmail.com>
2025-06-17 12:28:32 -0600

    Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -22,20 +22,20 @@ return {
   -- 		},
   -- 	},
   -- },
-  
+
 
 
   -- feeco's plugins
   -- file auto save
   {
-   "Pocco81/auto-save.nvim",
-   lazy=false,
-	 config = function()
-	  require("auto-save").setup {
-	 	-- your config goes here
-	 	-- or just leave it empty :)
-	  }
-	 end,
+    "Pocco81/auto-save.nvim",
+    lazy = false,
+    config = function()
+      require("auto-save").setup {
+        -- your config goes here
+        -- or just leave it empty :)
+      }
+    end,
   },
   {
     "prisma/vim-prisma",
