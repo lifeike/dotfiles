4ad90e702ffd4b176e1492fee056c67bda256ccf
Feeco <lifeike67@gmail.com>
2025-09-05 13:12:22 -0600

    Update .gitconfig Update .config/nvim/lua/configs/lspconfig.lua

--- private_dot_config/nvim/lua/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,48 @@
--- load defaults i.e lua_lsp
+-- load NvChad defaults (lua_ls etc)
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+-- List of servers to enable
+local servers = { "html", "cssls", "pyright", "ts_ls" }
+
+-- Setup each server with defaults
+for _, server in ipairs(servers) do
+  lspconfig[server].setup {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+-- Optional: Custom Pyright settings
+lspconfig.pyright.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+}
+
+-- Optional: Custom TypeScript/JavaScript settings
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    typescript = {
+      format = { enable = true },
+    },
+    javascript = {
+      format = { enable = true },
+    },
+  },
+}
