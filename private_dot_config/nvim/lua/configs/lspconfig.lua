106fce58a9cc262a27dffa51d7744db62cf3ed3b
Feeco <lifeike67@gmail.com>
2025-11-15 15:31:57 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/configs/lspconfig.lua

--- private_dot_config/nvim/lua/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,10 @@
 -- load NvChad defaults (lua_ls etc)
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+-- NEW: compatibility shim for Neovim 0.11+
+-- makes lspconfig.setup still work without errors
+local lspconfig = vim.lsp.config
+
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- List of servers to enable
@@ -21,7 +24,15 @@ lspconfig.pyright.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  settings = { python = { analysis = { typeCheckingMode = "basic", autoSearchPaths = true, useLibraryCodeForTypes = true, }, }, },
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
 }
 
 -- Optional: Custom TypeScript/JavaScript settings
@@ -30,6 +41,8 @@ lspconfig.ts_ls.setup {
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
   settings = {
-    typescript = { format = { enable = true }, },
-    javascript = { format = { enable = true }, }, },
+    typescript = { format = { enable = true } },
+    javascript = { format = { enable = true } },
+  },
 }
+
