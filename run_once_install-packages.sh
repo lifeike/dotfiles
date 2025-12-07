31ec1a4aa8ae3b7bfc5eb241322819c8c8e36226
Feeco <lifeike67@gmail.com>
2025-12-07 16:57:13 -0700

    Update install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -119,6 +119,7 @@ install_apt suckless-tools  # contains slock to lock screen
 install_apt brightnessctl # lock screen
 install_apt ncal
 install_apt tealdeer
+install_apt pipx
 # email neomut
 install_apt neomutt
 install_apt isync
