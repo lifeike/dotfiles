a807a9da0e55e081bee4d90aab07255fb85ac308
Feeco <lifeike67@gmail.com>
2025-12-13 11:35:12 -0700

    Update .config/nvim/lua/options.lua

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -5,3 +5,15 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline and row number!
 o.cursorcolumn =false   -- I tried vertical cursor line, finally disable it, cuz looks weird
+
+
+-- Open nvim-tree automatically when opening a directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
+
