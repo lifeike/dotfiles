3bf3603feef335e1810af12ec10bad36c69d86a0
Feeco <lifeike67@gmail.com>
2025-12-20 10:41:00 -0700

    Remove .config/nvim/lua/configs/luasnip.lua

--- private_dot_config/nvim/lua/configs/luasnip.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*/lua/snippets/*.lua",
-  callback = function()
-    require("luasnip").cleanup()
-    require("luasnip.loaders.from_lua").lazy_load()
-  end
-})
