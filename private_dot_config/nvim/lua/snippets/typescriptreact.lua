bab2124727135a4e130daa853f262d1fb6da6d42
Feeco <lifeike67@gmail.com>
2025-12-28 16:40:06 -0700

    Add .config/nvim/lua/configs/luasnip.lua Add .config/nvim/lua/plugins/luasnip.lua Add .config/nvim/lua/plugins/python-tool.lua Add .config/nvim/lua/snippets/html.lua Add .config/nvim/lua/snippets/typescriptreact.lua

---
+++ private_dot_config/nvim/lua/snippets/typescriptreact.lua
@@ -0,0 +1,10 @@
+return {
+ s("page", {
+    t({ "'use client'", "", "" }),
+    t({ "import React, { useState, useEffect } from 'react'", "", "" }),
+    t({ "export default () => {", "", "  return (" }),
+    t({ "    <div>" }),
+    t({ "      " }), i(1, "pageName"),
+    t({ "", "    </div>", "  )", "}" }),
+  }),
+}
