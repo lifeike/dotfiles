520b44943c377e2737a1b4fc36ea0eef5c2df6ce
Feeco <lifeike67@gmail.com>
2025-12-28 22:11:05 -0700

    Update .config/nvim/lua/plugins/6.aerial.lua

--- private_dot_config/nvim/lua/plugins/6.aerial.lua
+++ private_dot_config/nvim/lua/plugins/6.aerial.lua
@@ -7,8 +7,8 @@ return {
     filter_kind = false,                  -- show all symbols
     show_guides = true,
     layout = {
-      max_width = { 50, 0.2 },
-      min_width = 30,
+      max_width = { 40, 0.2 },
+      min_width = 20,
     },
   },
   -- Optional dependencies
