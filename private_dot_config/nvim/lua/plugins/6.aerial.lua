11a9eb8967b220ab5de0fec34db39a52ef9bff3d
Feeco <lifeike67@gmail.com>
2025-12-28 21:08:03 -0700

    Update .config/nvim/lua/plugins/6.aerial.lua

--- private_dot_config/nvim/lua/plugins/6.aerial.lua
+++ private_dot_config/nvim/lua/plugins/6.aerial.lua
@@ -3,12 +3,13 @@ return {
   event = "VeryLazy",
   opts = {
     -- Priority list of backends for aerial to use
-    backends = { "lsp", "treesitter", "markdown", "man" },
+    backends = { "lsp", "treesitter" },  -- default
+    filter_kind = false,                  -- show all symbols
+    show_guides = true,
     layout = {
       max_width = { 40, 0.2 },
       min_width = 20,
     },
-    show_guides = true,
   },
   -- Optional dependencies
   dependencies = {
