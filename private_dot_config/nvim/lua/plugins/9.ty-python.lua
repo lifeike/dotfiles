de251fc6fc7b16bb462efb6e76fcbacedab6a850
Feeco <lifeike67@gmail.com>
2025-12-28 20:50:33 -0700

    Add .config/nvim/lua/plugins/10.typescrip-tool.lua Add .config/nvim/lua/plugins/6.aerial.lua Add .config/nvim/lua/plugins/7.luasnip.lua Add .config/nvim/lua/plugins/8.render-markdown.lua Add .config/nvim/lua/plugins/9.ty-python.lua

---
+++ private_dot_config/nvim/lua/plugins/9.ty-python.lua
@@ -0,0 +1,31 @@
+return {
+  "neovim/nvim-lspconfig",
+  ft = { "python", "ipynb" },
+  config = function()
+    local lspconfig = require('lspconfig')
+    
+    -- Add ty server configuration
+    local configs = require('lspconfig.configs')
+    if not configs.ty then
+      configs.ty = {
+        default_config = {
+          cmd = { 'ty', 'server' },
+          filetypes = { 'python' },
+          root_dir = function(fname)
+            return lspconfig.util.root_pattern('pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git')(fname)
+              or lspconfig.util.path.dirname(fname)
+          end,
+          settings = {},
+        },
+      }
+    end
+    
+    lspconfig.ty.setup({
+      settings = {
+        ty = {}
+      }
+    })
+  end,
+}
+
+
