3386a0fd8f32648e38dc332b97cde42832c5605c
Feeco <lifeike67@gmail.com>
2026-01-06 18:32:49 -0700

    Update install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -113,7 +113,7 @@ system_update() {
     sudo apt upgrade -y
     sudo apt-get update -y
     sudo apt-get upgrade -y
-    sudo snap refresh -y
+    sudo snap refresh --hold # stop snap store updates
 }
 
 # Function to install a .deb package from URL
