60b77aa105d766067e85f5343fb73a1164635516
Feeco <lifeike67@gmail.com>
2026-03-02 19:15:25 -0700

    Update .config/nvim/lua/configs/conform.lua

--- private_dot_config/nvim/lua/configs/conform.lua
+++ private_dot_config/nvim/lua/configs/conform.lua
@@ -12,6 +12,9 @@ require("conform").setup {
     prettier = {
       prepend_args = { "--print-width", "200" },
     },
+    rustfmt = {
+      prepend_args = { "--config", "max_width=200" },
+    },
   },
   format_on_save = {
     timeout_ms = 5000, -- increase timeout to 5 seconds
